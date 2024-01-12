2f96e4ea904641c70e01b9c3bfa4430c2bd1531f
larissamato <matoslarissa31@gmail.com>
2024-01-12 12:47:57 -0300

    :sparkles: first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/world")]              
+fn world() -> &'static str {
+    "hello, estou testando"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![index])
+    .mount("/api", routes![world])
+}
